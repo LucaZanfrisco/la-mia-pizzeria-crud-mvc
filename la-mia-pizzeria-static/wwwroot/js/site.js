d79be49f25b95d0029e1282d7cb925d65d63cb68
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-10-02 15:39:41 +0200

    Script che mostra un recap del nuovo inserimento

--- la-mia-pizzeria-static/wwwroot/js/site.js
+++ la-mia-pizzeria-static/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let form = document.getElementById("nuovaPizza");
+
+form.addEventListener("keyup", function () {
+    let imgPath = document.getElementById("imgPath").value;
+    let imgPreview = document.getElementById("imgPreview");
+    imgPreview.src = imgPath;
+
+    let nome = document.getElementById("nome").value;
+    let nomePizza = document.getElementById("nomePizza");
+    if (nome !== '') {
+        nomePizza.innerText = nome;
+    } else {
+        nomePizza.innerText = 'Nome della pizza';
+    }
+
+    let ingredienti = document.getElementById("ingredienti").value;
+    let listaIngredienti = document.getElementById("listaIngredienti");
+    if (ingredienti !== '') {
+        listaIngredienti.innerText = ingredienti;
+    } else {
+        listaIngredienti.innerText = 'Ingredienti';
+    }
+
+    let prezzo = document.getElementById("prezzo").value;
+    let prezzoFinale = document.getElementById("prezzoFinale");
+    if (prezzo !== '') {
+        prezzoFinale.innerText = `${prezzo} €`;
+    } else {
+        prezzoFinale.innerText = 'Prezzo';
+    }
+})
